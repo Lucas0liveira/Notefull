2285c13bae7fc06debcf5fd8bea538b5207b3a18
Leozaru <leoufms@gmail.com>
2020-06-18 20:59:27 -0300

    Correções & xml-Tela_Notas

---
+++ app/src/main/java/com/example/notefull/NewNoteActivity.java
@@ -0,0 +1,15 @@
+package com.example.notefull;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
