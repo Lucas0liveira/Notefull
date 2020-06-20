263d7dbe5a3b8c2da2220479d96c09f0157ac698
Lucas0liveira <lucasoliveira96@outlook.com>
2020-06-20 19:51:28 -0400

    Correções banco de dados

--- app/src/main/java/com/example/notefull/EdtNotesActivity.java
+++ app/src/main/java/com/example/notefull/EdtNotesActivity.java
@@ -24,11 +24,9 @@ public class EdtNotesActivity extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         long id = -1;
-        long userId = -1;
-        if(b != null) {
+        final long userId = b.getLong("userId");
+        if (b != null)
             id = b.getInt("id");
-            userId = b.getLong("userId");
-        }
 
         createNotificationChannel();
 
@@ -43,12 +41,19 @@ public class EdtNotesActivity extends AppCompatActivity {
         Button btnConfirm = findViewById(R.id.btnNoteCheck);
         Button btnQuit = findViewById(R.id.btnNoteQuit);
 
-        btnNotification.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        DatabaseHelper db = new DatabaseHelper(this);
+        Note note = db.getNote(id);
+
+        edtTitulo.setText(note.getTitle());
+        edtBody.setText(note.getBody());
+        edtLembrete.setText(note.getTimer());
+
+        btnNotification.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 Toast.makeText(EdtNotesActivity.this, "Lembrete Definido!", Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(EdtNotesActivity.this, Lembrete.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(EdtNotesActivity.this,0,intent,0);
+                Intent intent = new Intent(view.getContext(), Lembrete.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(view.getContext(), 0, intent, 0);
 
                 AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
@@ -57,43 +62,42 @@ public class EdtNotesActivity extends AppCompatActivity {
                 long timeSeconds = 1000 * 1;
 
                 alarmManager.set(AlarmManager.RTC_WAKEUP, timeAtButtonClick + timeSeconds, pendingIntent);
-
-
             }
         });
 
 
-                final long finalId = id;
-                btnConfirm.setOnClickListener(new View.OnClickListener(){
-                    @RequiresApi(api = Build.VERSION_CODES.O)
-                    public void onClick(View view){
-                        DatabaseHelper db = new DatabaseHelper(view.getContext());
-                        db.RemoveNote(finalId);
-                        Note note = new Note();
-                        note.setTitle(edtTitulo.getText().toString());
-                        note.setBody(edtBody.getText().toString());
-                        note.setTimer(edtLembrete.getText().toString());
-                        note.setLat(edtLocal.getText().toString());
-                        note.setLg(edtLocal.getText().toString());
-                        note.setDate(LocalDateTime.now().toString());
-                        db.addNote(note);
-                        Toast.makeText(EdtNotesActivity.this, "Nota criada com sucesso.", Toast.LENGTH_SHORT).show();
+        final long finalId = id;
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            public void onClick(View view) {
+                DatabaseHelper db = new DatabaseHelper(view.getContext());
+                db.RemoveNote(finalId);
+                Note note = new Note();
+                note.setTitle(edtTitulo.getText().toString());
+                note.setBody(edtBody.getText().toString());
+                note.setTimer(edtLembrete.getText().toString());
+                note.setLat(edtLocal.getText().toString());
+                note.setLg(edtLocal.getText().toString());
+                note.setDate(LocalDateTime.now().toString());
+                db.addNote(note, userId);
+                Toast.makeText(EdtNotesActivity.this, "Nota criada com sucesso.", Toast.LENGTH_SHORT).show();
 
             }
         });
 
-        btnQuit.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), NoteActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
     }
-    private void createNotificationChannel(){
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            CharSequence name= "Lembrete";
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Lembrete";
             String description = "Canal de lembrete";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel("notify", name, importance);
--- app/src/main/java/com/example/notefull/Lembrete.java
+++ app/src/main/java/com/example/notefull/Lembrete.java
@@ -3,6 +3,7 @@ package com.example.notefull;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,6 +12,11 @@ public class Lembrete extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Bundle b = intent.getExtras();
+        final long userId = b.getLong("userId");
+        final long id = b.getLong("id");
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notify")
                 .setSmallIcon(R.mipmap.ic_notification)
                 .setContentTitle("Notefull")
@@ -19,6 +25,7 @@ public class Lembrete extends BroadcastReceiver {
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
+        Intent myIntent = new Intent(context, NoteActivity.class);
        notificationManager.notify(200, builder.build());
     }
 }
